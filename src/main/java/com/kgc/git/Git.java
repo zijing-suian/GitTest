5f71dbdcce5a8d4397ac8b777891f4f4b23e23a9
wq <244289393@qq.com>
2022-03-21 18:01:25 +0800

    master four commit

--- src/main/java/com/kgc/git/Git.java
+++ src/main/java/com/kgc/git/Git.java
@@ -6,5 +6,6 @@ public class Git {
         System.out.println("修改");
         System.out.println("再次修改!");
         System.out.println("hot-fix 修改!");
+        System.out.println("master test!");
     }
 }
